1bee40978fb7e74a89d314338881facc9261f865
ul88 <hkimog13@naver.com>
2025-11-02 22:53:17 +0900

    shell 입력으로 변경

--- compile.sh
+++ compile.sh
@@ -4,28 +4,27 @@ path="./marking/resource"
 
 file=$(find "$path" -maxdepth 1 -type f -name "main.*" | head -n 1)
 
-problem_id=$1
-user_id=$2
+ext=$1
+problem_id=$2
+user_id=$3
 
-if [ -n "$file" ]; then
-    echo "파일 존재함"
-    echo "파일 이름: $file"
+file=./marking/resource/main.$ext
 
-    ext="${file##*.}"
-    echo "확장자 이름 : $ext"
+cat > $file
 
-    case $ext in
-        "c") 
-            gcc $file -o "$path/a.out"
-            cd ./marking/build
-            ./program $ext $problem_id $user_id
-            ;;
-        "cpp") 
-            g++ $file -o "$path/a.out"
-            cd ./marking/build
-            ./program $ext $problem_id $user_id
-            ;;
-    esac
-else
-    echo "파일 없음"
-fi
+case $ext in
+    "c") 
+        gcc $file -o "$path/a.out"
+        cd ./marking/build
+        ./program $ext $problem_id $user_id
+        ;;
+    "cpp") 
+        g++ $file -o "$path/a.out"
+        cd ./marking/build
+        ./program $ext $problem_id $user_id
+        ;;
+    "python")
+        cd ./marking/build
+        ./program $ext $program_id $user_id
+        ;;
+esac
--- test.cpp
+++ test.cpp
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello World!");
+    printf("HelloWorld!");
     return 0;
 }
